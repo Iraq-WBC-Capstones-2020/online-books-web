44ee8a03f73ba8b66a3f5dc2f32d4605a30377d5
Qays <work.worker55@gmail.com>
2020-07-28 17:41:27 +0300

    Adding test to spinner componenets

---
+++ src/componenets/Spinner/__test__/Spinner.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Spinner from './../Spinner';
+
+test('renders learn react link', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Spinner />, div);
+});
