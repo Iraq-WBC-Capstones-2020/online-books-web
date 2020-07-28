68ca263fe1570cb5cabd455f0433f3cf31ecf5c8
Qays <work.worker55@gmail.com>
2020-07-28 17:41:27 +0300

    Fixing a typo in spinner test

--- src/componenets/Spinner/__test__/Spinner.test.js
+++ src/componenets/Spinner/__test__/Spinner.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import Spinner from './../Spinner';
 
-test('renders learn react link', () => {
+test('renders spinner', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Spinner />, div);
 });
