d99e61e42b33347963c2deaa58d0753c696aab34
sara mohammed <sara.korea96@gmail.com>
2020-07-31 13:54:30 +0300

    apply the feedback comments with improvements

--- src/pages/AboutUs/Details.js
+++ src/pages/AboutUs/Details.js
@@ -1,44 +1,38 @@
 import React from 'react';
 import pic from './pic.png';
-import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
+import { MDBRow, MDBCol } from 'mdbreact';
 
 function Details() {
+  const aboutPageEnglishTranslation = {
+    headerTitle: 'About us',
+    text:
+      'It is a long established fact that a reader will be distracted bythe readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using  content hereh. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of etters, as opposed to using Content h ereh.',
+    ReadMoreText: 'Read more',
+  };
   return (
-    <MDBContainer className="bg-gray-100">
+    <div className="bg-gray-100 col">
       <MDBRow>
         <MDBCol lg="5">
-          <img src={pic} alt="artwork of a girl" />
+          <img src={pic} alt="artwork of a girl reading a book" />
         </MDBCol>
         <MDBCol>
-          <div className="mt-40">
-            <h3 className="font-weight-bold  p-0  text-3xl ml-1">
-              <strong>About us :</strong>
+          <div className="mt-48">
+            <h3 className="font-weight-bold text-3xl ml-1">
+              {aboutPageEnglishTranslation.headerTitle} :
             </h3>
-            <hr className="col-lg-2 h-2 bg-blue-500  " />
-            <p className="mt-2 text-lg">
-              It is a long established fact that a reader will be distracted by
-              the readable content <br />
-              of a page when looking at its layout. The point of using Lorem
-              Ipsum is that it
-              <br /> has a more-or-less normal distribution of letters, as
-              opposed to using <br />
-              Content hereh. It is a long established fact that a reader will be
-              distracted by the <br />
-              readable content of a page when looking at its layout. The point
-              of using Lorem <br />
-              Ipsum is that it has a more-or-less normal distribution of
-              letters, as opposed to using Content hereh.
+            <hr className="col-lg-2 h-2 bg-blue-500" />
+            <p className="mt-2 mr-5 text-lg text-justify">
+              {aboutPageEnglishTranslation.text}
             </p>
-
-            <div className="">
-              <button className="rounded-full h-8 w-1/9 mt-3 transition duration-500 ease-in-out bg-blue-500 hover:bg-blue-700 transform hover:-translate-y-1 hover:scale-110 ...">
-                <span className="text-white">Read more</span>
-              </button>
-            </div>
+            <button className="rounded-full h-8 w-1/9 mt-3 transition duration-500 ease-in-out bg-blue-500 hover:bg-blue-700 transform hover:-translate-y-1 hover:scale-110">
+              <span className="text-white">
+                {aboutPageEnglishTranslation.ReadMoreText}
+              </span>
+            </button>
           </div>
         </MDBCol>
       </MDBRow>
-    </MDBContainer>
+    </div>
   );
 }
 
