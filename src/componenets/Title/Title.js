155ff3b9ce1a2cc2fc1cd3cb7d8485e592e64d96
Qays <work.worker55@gmail.com>
2020-08-01 20:56:20 +0300

    Adding title and paragraph components

---
+++ src/componenets/Title/Title.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { } from "mdbreact";
+
+export default function Title(props) {
+    const title = props.title;
+    return (
+    <h1 className="">{title}</h1>
+    );
+}
