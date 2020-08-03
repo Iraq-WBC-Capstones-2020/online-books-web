04e6b98d0446fd497a5ce6709d1ddbf1f1b444c6
Neve bedr <neven2225@gmail.com>
2020-08-03 22:39:38 +0300

    fixing lint again

--- src/pages/BookPage/AudioSectionParagraph.js
+++ src/pages/BookPage/AudioSectionParagraph.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.scss';
 import bookCover from './bookCover.jpg';
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon } from 'mdbreact';
+import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
 const AudioSection = () => {
   return (
     <MDBContainer className="mt-5">
@@ -13,12 +13,12 @@ const AudioSection = () => {
         </MDBCol>
         <MDBCol sm="6" size="12">
           <div className="mt-5 ">
-            <h1 className="blue-text"> Mind without body</h1>
+            <h1 className="blue-text">Mind without body</h1>
             <p className="mt-3  paragraph">
-              is simply dummy text of the printing and typesetting industry.
+              {`is simply dummy text of the printing and typesetting industry.
               Lorem Ipsum has been the industry's standard dummy text ever since
               the 1500s, when an unknown printer took a galley of type and
-              scrambled it to make a type specimen book.
+              scrambled it to make a type specimen book.`}
             </p>
           </div>
         </MDBCol>
--- src/pages/BookPage/bookPageWithoutLogin.js
+++ src/pages/BookPage/bookPageWithoutLogin.js
@@ -19,11 +19,11 @@ const BlogPage = () => {
               <MDBIcon icon="star"></MDBIcon>
             </div>
             <p className="mt-3 paragraph">
-              is simply dummy text of the printing and typesetting industry.
+              {`is simply dummy text of the printing and typesetting industry.
               Lorem Ipsum has been the industry's standard dummy text ever since
               the 1500s, when an unknown printer took a galley of type and
               scrambled it to make a type specimen book.galley of type and
-              scrambled it to make a type specimen book.
+              scrambled it to make a type specimen book.`}
             </p>
             <span className="gray-text">Language:Arabic</span>
             <br />
