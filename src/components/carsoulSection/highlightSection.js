b694ef49e77c3bb9756c6d2b89d7960629e8acc3
zeena kareem <zena342699@gmail.com>
2020-08-03 13:55:33 +0300

    fix props

--- src/components/carsoulSection/highlightSection.js
+++ src/components/carsoulSection/highlightSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function HighLight() {
+function HighLight(props) {
   return (
     <div className="buttomText bg-black  pt-1 pt-md-2 text-white font-bold text-center  highlight ">
       {props.text}
