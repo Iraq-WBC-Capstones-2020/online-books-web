4fc45b4d3a0b38598eecaeb174a3a9bc72671082
Zainab <zai99nb@gmail.com>
2020-08-03 16:53:25 +0300

    run lint fixing

--- src/components/paymentTable/paymentTable.js
+++ src/components/paymentTable/paymentTable.js
@@ -1,70 +1,82 @@
 import React from 'react';
 import './paymentTable.scss';
-import { MDBContainer,MDBRow,MDBCol,MDBTable, MDBTableBody, MDBTableHead } from 'mdbreact';
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBTable,
+  MDBTableBody,
+  MDBTableHead,
+} from 'mdbreact';
 
-export default function PaymentTable(){
-    const books =  [{
-          title:"book1",
-          copies: 1 ,
-          price : "$",
-        },
-        {
-          title:"book2",
-          copies: 1 ,
-          price : "$",
-        },
-        {
-          title:"book3",
-          copies: 1 ,
-          price : "$",
-        }
-      ];
-      const paymentTable = {
-        booksOrder : 'Your Books Order',
-        books :'Books',
-        copies :'Copies',
-        price :'Price',
-        total :'Total',
-      }
-        
+export default function PaymentTable() {
+  const books = [
+    {
+      title: 'book1',
+      copies: 1,
+      price: '$',
+    },
+    {
+      title: 'book2',
+      copies: 1,
+      price: '$',
+    },
+    {
+      title: 'book3',
+      copies: 1,
+      price: '$',
+    },
+  ];
+  const paymentTable = {
+    booksOrder: 'Your Books Order',
+    books: 'Books',
+    copies: 'Copies',
+    price: 'Price',
+    total: 'Total',
+  };
+
   return (
     <MDBContainer>
       <MDBRow>
-      <MDBCol md="4">
-        <div className="text-center">
-   <p className="h5 mt-5 gray-text">{paymentTable.booksOrder}</p>
-   <hr className="border-4 my-3 mx-5 bg-primary"></hr>
-   </div>
-    <MDBTable >
-      <MDBTableHead  >
-        <tr>
-          <th></th>
-          <th>{paymentTable.books}</th>
-          <th>{paymentTable.copies}</th>
-          <th>{paymentTable.price}</th>
-        </tr>
-      </MDBTableHead>
-      <MDBTableBody>
-        {books.map(book=>{
-          return (
-            <tr>
-            <td ><input type="checkbox"/></td>
-            <td>{book.title}</td>
-            <td>{book.copies}</td>
-            <td>{book.price}</td>
-            </tr>
-              )
-        })}
-        <tr>
-            <td></td>
-            <td><p className ="h6">{paymentTable.total}</p></td>
-            <td>{books.copies}</td>
-            <td>{books.price}</td>
-            </tr>
-      </MDBTableBody>
-    </MDBTable>
-    </MDBCol>
-    </MDBRow>
+        <MDBCol md="4">
+          <div className="text-center">
+            <p className="h5 mt-5 gray-text">{paymentTable.booksOrder}</p>
+            <hr className="border-4 my-3 mx-5 bg-primary"></hr>
+          </div>
+          <MDBTable>
+            <MDBTableHead>
+              <tr>
+                <th></th>
+                <th>{paymentTable.books}</th>
+                <th>{paymentTable.copies}</th>
+                <th>{paymentTable.price}</th>
+              </tr>
+            </MDBTableHead>
+            <MDBTableBody>
+              {books.map((book) => {
+                return (
+                  <tr>
+                    <td>
+                      <input type="checkbox" />
+                    </td>
+                    <td>{book.title}</td>
+                    <td>{book.copies}</td>
+                    <td>{book.price}</td>
+                  </tr>
+                );
+              })}
+              <tr>
+                <td></td>
+                <td>
+                  <p className="h6">{paymentTable.total}</p>
+                </td>
+                <td>{books.copies}</td>
+                <td>{books.price}</td>
+              </tr>
+            </MDBTableBody>
+          </MDBTable>
+        </MDBCol>
+      </MDBRow>
     </MDBContainer>
   );
 }
