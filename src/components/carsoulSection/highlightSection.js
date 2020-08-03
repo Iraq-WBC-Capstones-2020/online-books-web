d90d882a3aa83a622d28ceec78b189d99fd235ca
zeena kareem <zena342699@gmail.com>
2020-08-03 13:46:59 +0300

    fix props

--- src/components/carsoulSection/highlightSection.js
+++ src/components/carsoulSection/highlightSection.js
@@ -3,7 +3,7 @@ import React from 'react';
 function HighLight() {
   return (
     <div className="buttomText bg-black  pt-1 pt-md-2 text-white font-bold text-center  highlight ">
-      {/*props.text*/}best sellers in books
+      {props.text}
     </div>
   );
 }
