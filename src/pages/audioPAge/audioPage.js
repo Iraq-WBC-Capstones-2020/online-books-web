22cea6679486ad6770f09c36ef28e82d4e59309f
Neve bedr <neven2225@gmail.com>
2020-08-04 17:56:41 +0300

    Creating audio paragraph

---
+++ src/pages/audioPAge/audioPage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './style.css';
+import bookCover from './img/bookCover.jpg';
+import './style.scss';
+import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
+
+const AudioPage = () => {
+  return (
+    <MDBContainer className="mt-5">
+      <MDBRow>
+        <MDBCol sm="6" size="12">
+          <div className="mt-5  ">
+            <img src={bookCover} alt="Book cover" width="353" height="498" />
+          </div>
+        </MDBCol>
+        <MDBCol sm="6" size="12">
+          <div className="mt-5">
+            <h1 className="blue-text"> Mind without body</h1>
+            <p className="mt-3 paragraph">
+              is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book.galley of type and
+              scrambled it to make a type specimen book.
+            </p>
+          </div>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+};
+
+export default AudioPage;
