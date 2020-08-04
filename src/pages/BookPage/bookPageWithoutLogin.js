257a613b703eca57cc53f105cddf0cca9c3d1ba3
Neve bedr <neven2225@gmail.com>
2020-08-04 11:28:20 +0300

    EditingBookPadeFile

--- src/pages/BookPage/bookPageWithoutLogin.js
+++ src/pages/BookPage/bookPageWithoutLogin.js
@@ -2,14 +2,21 @@ import React from 'react';
 import './style.scss';
 import bookCover from './bookCover.jpg';
 import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon } from 'mdbreact';
-const BlogPage = () => {
+import { useTranslation } from 'react-i18next';
+const BookPage = () => {
+  const { t } = useTranslation();
+  const bookName = 'Mind Without Body';
+  const authortName = 'Ahmed khaled tofeq';
+  const bookType = 'Fantasy';
+  const publishDate = '2017';
+  const isLoggedIn = false;
   return (
     <MDBContainer className="mt-5">
       <MDBRow>
         <MDBCol sm="6" size="12">
           <div className="mt-5">
-            <h1 className="blue-text"> Mind without body</h1>
-            <span className="gray-text">Ahmed khaled tofeq</span>
+            <h1 className="blue-text"> {bookName}</h1>
+            <span className="gray-text">{authortName}</span>
             <br />
             <div className="mt-2 yellow-star">
               <MDBIcon icon="star"></MDBIcon>
@@ -25,26 +32,52 @@ const BlogPage = () => {
               scrambled it to make a type specimen book.galley of type and
               scrambled it to make a type specimen book.`}
             </p>
-            <span className="gray-text">Language:Arabic</span>
-            <br />
+            <span className="gray-text">{t('language')}:Arabic</span>
+          </div>
+          <br />
+          {!isLoggedIn && (
+            <>
+              <span className="gray-text">
+                Price: <strong>15$</strong>
+              </span>
 
-            <span className="gray-text">
-              Price: <strong>15$</strong>
-            </span>
-          </div>
-          <div className="mt-3">
-            <MDBBtn color="info" className="mr-3 px-3 py-0">
-              Fantasy
-            </MDBBtn>
-            <MDBBtn color="info" className="px-3 py-0">
-              2017
-            </MDBBtn>
-          </div>
-          <div className="mt-4">
-            <MDBBtn color="primary" className="px-4 rounded-pill">
-              Add to cart
-            </MDBBtn>
-          </div>
+              <div className="mt-3">
+                <MDBBtn color="info" className="mr-3 px-3 py-0">
+                  {bookType}
+                </MDBBtn>
+                <MDBBtn color="info" className="px-3 py-0">
+                  {publishDate}
+                </MDBBtn>
+              </div>
+              <div className="mt-4">
+                <MDBBtn color="primary" className="px-4 rounded-pill">
+                  {t('addToCart')}
+                </MDBBtn>
+              </div>
+            </>
+          )}
+          {isLoggedIn && (
+            <>
+              <div className="mt-3">
+                <MDBBtn color="info" className="mr-3 px-3 py-0">
+                  {bookType}
+                </MDBBtn>
+                <MDBBtn color="info" className="px-3 py-0">
+                  {publishDate}
+                </MDBBtn>
+              </div>
+              <div className="mt-4">
+                <MDBIcon
+                  outline
+                  className="rounded-circle"
+                  color="black"
+                  icon="headphones-alt"
+                  size="4x"
+                />
+                <MDBIcon className="ml-5" icon="arrow-down" size="4x" />
+              </div>
+            </>
+          )}
         </MDBCol>
 
         <MDBCol sm="6" size="12">
@@ -57,4 +90,4 @@ const BlogPage = () => {
   );
 };
 
-export default BlogPage;
+export default BookPage;
