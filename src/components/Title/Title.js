b45ff3fd1744f91596c53d817c095b1f3fa21492
Qays <work.worker55@gmail.com>
2020-08-05 21:37:09 +0300

    Implementing AuthorPage

--- src/components/Title/Title.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { } from "mdbreact";
-
-export default function Title(props) {
-    const title = props.title;
-    return (
-    <h1 className="">{title}</h1>
-    );
-}
