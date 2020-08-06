ebda917d3577b82e13eac9ac9a14fdada426062c
Qays <work.worker55@gmail.com>
2020-08-07 01:32:01 +0300

    Adding svg

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
-
 const Footer = () => {
-    return ( <></> );
-}
- 
+  return (
+    <footer className="w-full">
+      <svg
+      className="h-auto"
+        viewBox="0 0 1440 653"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M345.185 157.161C146.86 151.813 32.8747 60.2452 0 0V653H1440V327.866C1420.4 333.321 1399.19 335.723 1376.26 334.517C1291.08 330.039 1273.15 212.168 1102.8 236.291C424.32 332.369 636.574 165.019 345.185 157.161Z"
+          fill="#F9F9F9"
+        />
+      </svg>
+    </footer>
+  );
+};
+
 export default Footer;
