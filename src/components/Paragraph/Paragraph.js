83be760db18a7d4affca9c1bb9a2d2add375c9e0
Qays <work.worker55@gmail.com>
2020-08-09 03:37:36 +0300

    Running test and fixing lint

--- src/components/Paragraph/Paragraph.js
+++ src/components/Paragraph/Paragraph.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Paragraph ({ title, children }) {
+function Paragraph({ title, children }) {
   return (
     <>
       <p className="text-3xl font-medium border-b-4 inline border-blue-500">
@@ -9,6 +9,6 @@ function Paragraph ({ title, children }) {
       <p className="text-base leading-loose text-justify">{children}</p>
     </>
   );
-};
+}
 
 export default Paragraph;
--- src/components/Paragraph/__test__/Paragraph.test.js
+++ src/components/Paragraph/__test__/Paragraph.test.js
@@ -3,9 +3,11 @@ import { render } from '@testing-library/react';
 import Paragraph from '../Paragraph';
 
 test('renders Paragraph', () => {
-    const title = "Title";
-    const paragraph= "Paragraph";
-  const { getByText } = render(<Paragraph title={title} >{paragraph}</Paragraph>);
+  const title = 'Title';
+  const paragraph = 'Paragraph';
+  const { getByText } = render(
+    <Paragraph title={title}>{paragraph}</Paragraph>
+  );
   const titleTextElement = getByText(/Title/i);
   const paragraphTextElement = getByText(/Paragraph/i);
   expect(titleTextElement).toBeInTheDocument();
