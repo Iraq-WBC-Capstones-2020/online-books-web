dbdc210220a269ee27ef34c3e9f0d906b3bc045c
Qays <work.worker55@gmail.com>
2020-08-09 18:48:39 +0300

    Passing className as prop to the Paragraph components

--- src/components/Paragraph/Paragraph.js
+++ src/components/Paragraph/Paragraph.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-function Paragraph({ title, children }) {
+function Paragraph({ title, className, children }) {
   return (
     <>
-      <p className="text-3xl font-medium border-b-4 inline border-blue-500">
+      <p
+        className={`text-3xl font-medium border-b-4 inline border-blue-500 ${className}`}
+      >
         {title}
       </p>
-      <p className="text-base leading-loose text-justify">{children}</p>
+      <p className={`text-base leading-loose text-justify ${className}`}>
+        {children}
+      </p>
     </>
   );
 }
