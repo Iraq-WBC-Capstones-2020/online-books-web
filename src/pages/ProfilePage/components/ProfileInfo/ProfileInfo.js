2e1d5c2ff0c9acf7599840c467fff0a5f4336c5e
Qays <work.worker55@gmail.com>
2020-08-10 06:16:13 +0300

    Adding mock data and structure

--- src/pages/ProfilePage/components/ProfileInfo/ProfileInfo.js
+++ src/pages/ProfilePage/components/ProfileInfo/ProfileInfo.js
@@ -1,27 +1,68 @@
-import React from 'react';
-import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
+import React, { useState } from 'react';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdbreact';
 import { useTranslation } from 'react-i18next';
 
 function ProfileInfo({ profileInfo }) {
-    const { t } = useTranslation();
-
+  const { t } = useTranslation();
+  const [isEdit, setIsEdit] = useState(false);
+  const basicInfo = {
+    name: profileInfo.name,
+    gender: profileInfo.gender,
+    birthday: profileInfo.birthday,
+    city: profileInfo.address.city,
+    country: profileInfo.address.country,
+  };
+  const contactInfo = {
+    address: profileInfo.address.street,
+    phoneNumber: profileInfo.phoneNumber,
+    email: profileInfo.email,
+  };
   return (
-    <MDBContainer>
-      <MDBRow className="h-auto">
-        <MDBCol lg="3" md="6" sm="12" size="12" className="py-4"></MDBCol>
-        <MDBCol
-          lg="8"
-          md="6"
-          sm="12"
-          size="12"
-          className="py-4 text-center text-md-left"
-        >
-          <h4 className="text-3xl font-medium text-blue-500 pb-3">
-            {profileInfo.name}
-          </h4>
+    <>
+      <MDBBtn
+        onClick={() => setIsEdit((isEdit) => !isEdit)}
+        className="shadow-none text-blue-500 p-0 m-0 float-right mr-2 capitalize"
+        color="transparent"
+      >
+        {t('profilePage.edit')}
+      </MDBBtn>
+      <MDBRow>
+        <MDBCol md="6" sm="12" size="12">
+          <h3 className="text-2xl leading-loose font-semibold">
+            {t('profilePage.basicInfo')}
+          </h3>
+          {Object.entries(basicInfo).map(([key, value]) => (
+            <MDBRow key={key}>
+              <MDBCol md="4" sm="6" size="6">
+                <h3 className="text-base leading-loose">
+                  {t(`profilePage.${key}`)}:
+                </h3>
+              </MDBCol>
+              <MDBCol md="8" sm="6" size="6">
+                <h3 className="text-base leading-loose">{value}</h3>
+              </MDBCol>
+            </MDBRow>
+          ))}
+        </MDBCol>
+        <MDBCol md="6" sm="12" size="12">
+          <h3 className="text-2xl leading-loose font-semibold">
+            {t('profilePage.contactInfo')}
+          </h3>
+          {Object.entries(contactInfo).map(([key, value]) => (
+            <MDBRow key={key}>
+              <MDBCol md="4" sm="6" size="6">
+                <h3 className="text-base leading-loose">
+                  {t(`profilePage.${key}`)}:
+                </h3>
+              </MDBCol>
+              <MDBCol md="8" sm="6" size="6">
+                <h3 className="text-base leading-loose">{value}</h3>
+              </MDBCol>
+            </MDBRow>
+          ))}
         </MDBCol>
       </MDBRow>
-    </MDBContainer>
+    </>
   );
 }
 
