29eb31d0899ffdf33a5b78ec8022076d66be7d57
sara mohammed <sara.korea96@gmail.com>
2020-08-10 17:22:51 +0300

    deleting the extra spaces

--- src/components/contactUsPage/SideBar.js
+++ src/components/contactUsPage/SideBar.js
@@ -13,9 +13,9 @@ function ContactInformation() {
           </h1>
           <div
             className="flex justify-start animate transition duration-500 ease-in-out  
-                        transform hover:-translate-y-1 hover:scale-110  m-4 "
+                        transform hover:-translate-y-1 hover:scale-110 m-4"
           >
-            <i className="fas fa-map-marker-alt  text-4xl white-text ml-16 "></i>
+            <i className="fas fa-map-marker-alt text-4xl white-text ml-16"></i>
             <p className="text-gray-400 ml-4">
               A108 Adam Street
               <br /> New York NY 535022
@@ -23,7 +23,7 @@ function ContactInformation() {
           </div>
           <div
             className="flex justify-start animate transition duration-500 ease-in-out  
-                        transform hover:-translate-y-1 hover:scale-110  m-4 "
+                        transform hover:-translate-y-1 hover:scale-110 m-4"
           >
             <i className="fas fa-envelope pr-2 text-4xl white-text ml-16"></i>
             <p className="text-gray-400 ml-2">
@@ -34,9 +34,9 @@ function ContactInformation() {
           </div>
           <div
             className="flex justify-start animate transition duration-500 ease-in-out  
-                        transform hover:-translate-y-1 hover:scale-110  m-4 "
+                        transform hover:-translate-y-1 hover:scale-110 m-4"
           >
-            <i className="fas fa-phone pr-2 text-4xl  white-text ml-16"></i>
+            <i className="fas fa-phone pr-2 text-4xl white-text ml-16"></i>
             <p className="text-gray-400 ml-2 mb-24">
               + 01 234 567 89 <br />
               +1 5589 55488 51
