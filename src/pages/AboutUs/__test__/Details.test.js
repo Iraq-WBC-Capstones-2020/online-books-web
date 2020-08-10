921f166430a79b63a34c02a2ee97a6ebf4ba576e
sara-mohammed96 <63727924+sara-mohammed96@users.noreply.github.com>
2020-08-10 20:20:58 +0300

    Delete Details.test.js

--- src/pages/AboutUs/__test__/Details.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Details from './../Details';
-
-test('Details snapshot', () => {
-  const tree = renderer.create(<Details />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
