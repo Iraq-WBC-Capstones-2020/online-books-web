ae0c9df342932d0aa37e0037f32ee06baa3d93f0
Dunia <dunia.nasr777@gmail.com>
2020-08-11 00:18:22 +0300

    login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './i18n';
 import { HashRouter, Route } from 'react-router-dom';
-import FormPage from './pages/log';
+
 function App() {
   return (
     <HashRouter>
-      <FormPage />
       <Route exact path="/">
         <h1>Online Book</h1>
       </Route>
