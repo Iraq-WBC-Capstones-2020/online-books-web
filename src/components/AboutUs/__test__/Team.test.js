8f79de2bc19181fdfe16bebb6fe3b4e5ced8e410
sara mohammed <sara.korea96@gmail.com>
2020-08-11 19:41:33 +0300

    fixing the import

--- src/components/AboutUs/__test__/Team.test.js
+++ src/components/AboutUs/__test__/Team.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import TeamMembersInformation from '../Team';
+import TeamMembersInformation from './../Team';
 
 test('Team snapshot', () => {
   const tree = renderer.create(<TeamMembersInformation />).toJSON();
