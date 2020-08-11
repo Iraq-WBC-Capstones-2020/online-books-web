cd9359cbc6bf32dfcb9624928fa82f97a7ed9e5f
sara mohammed <sara.korea96@gmail.com>
2020-08-11 17:58:37 +0300

    test tryout

--- src/components/AboutUsPage/__test__/Services.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Services from './../Services';
-
-test('Services snapshot', () => {
-  const tree = renderer.create(<Services />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Services from './components/AboutUsPage/Services';
-import Detailsclient from './components/AboutUsPage/Clients';
+import DetailsClient from './components/AboutUsPage/Clients';
 import Details from './components/AboutUsPage/Details';
 import TeamMembersInformation from './components/AboutUsPage/Team';
 function AboutUsPageMain() {
@@ -9,7 +9,7 @@ function AboutUsPageMain() {
       <Details />
       <TeamMembersInformation />
       <Services />
-      <Detailsclient />
+      <DetailsClient />
     </>
   );
 }
