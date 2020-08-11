8a85660a1ba3da67cdff20749ad611e59029ca31
Neven bedr <neven2225@gmail.com>
2020-08-11 15:22:52 +0300

    Adding classes to div

--- src/components/MainPage/ShowCards.js
+++ src/components/MainPage/ShowCards.js
@@ -18,41 +18,37 @@ const ProjectsPage = () => {
     oldPrice: '$32.75',
   };
   return (
-    <MDBContainer className="">
+    <MDBContainer>
       <MDBRow className="mt-5">
-        <MDBCol sm="3" size="12" className="md-0 mb-4 ">
+        <MDBCol sm="3" size="12">
           <MDBCard
-            className="card-image "
+            className="card-image"
             style={{
-              width: '250px',
               backgroundImage:
                 'url(https://mdbootstrap.com/img/Photos/Horizontal/Nature/6-col/img%20%2873%29.jpg)',
             }}
           >
-            <div className=" mt-5 py-5 px-4 ">
-              <div className="mt-5 text-center">
-                <h3 className="font-weight-bold mt-5 text-3xl">
-                  <strong>Kevin Kwan</strong>
-                </h3>
-                <CreateStars />
+            <div className=" text-center position text-white">
+              <h3 className="font-weight-bold mt-2 text-xl">
+                <strong>Kevin Kwan</strong>
+              </h3>
+              <CreateStars />
 
-                <p className="pb-3 text-xl">
-                  {`${BookInfo.bookName}: ${BookInfo.bookType}`}
-                </p>
-              </div>
+              <p className="pb-3 text-xl">
+                {`${BookInfo.bookName}: ${BookInfo.bookType}`}
+              </p>
             </div>
             <div
               style={{
                 width: '100%',
               }}
-              className="rounded-bottom d-flex justify-content-around align-items-center absolute bottom-0 dark text-center text-white
-              "
+              className="d-flex justify-content-around align-items-center absolute bottom-0 dark text-center text-white"
             >
               <p className="text-xl">{BookInfo.newPrice}</p>
-              <p className="text-xl ml-3">
+              <p className="text-xl ">
                 <del>{BookInfo.oldPrice}</del>
               </p>
-              <div className="ml-4">
+              <div className=" border">
                 <MDBIcon size="2x" icon="cart-plus" />
               </div>
             </div>
