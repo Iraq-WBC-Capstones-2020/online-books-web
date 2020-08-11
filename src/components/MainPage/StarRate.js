7057a586025afb4147f5f6c67e7b838308b15996
Neven bedr <neven2225@gmail.com>
2020-08-11 14:02:50 +0300

    Adding star rate

---
+++ src/components/MainPage/StarRate.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function CreateStars({ rate }) {
+  let stars = [];
+  let firstDigit = rate - parseInt(rate);
+  let secondDigit = firstDigit.toString().split('.')[1];
+  let createStars = () => {
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rate) {
+        stars.push(<icon className="fa fa-star text-blue-300 mr-1 "></icon>);
+      } else {
+        if (secondDigit === 5) {
+          secondDigit = 0;
+          stars.push(
+            <icon className="fas fa-star-half-alt text-blue-300 mr-1 "></icon>
+          );
+        } else {
+          stars.push(
+            <icon className="fa fa-star text-blue-300 mr-1 font-light "></icon>
+          );
+        }
+      }
+    }
+    return stars;
+  };
+  return <>{createStars()}</>;
+}
+CreateStars.propTypes = {
+  rate: PropTypes.string,
+};
+
+export default CreateStars;
