b49345120bbfa4692c21f1abaefd0332269d9613
zeena kareem <zena342699@gmail.com>
2020-08-12 14:55:56 +0300

    __

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './i18n';
-import Main from './pages/Main/Main';
+
 function App() {
   return (
     <div>
       <h1>Online Books</h1>
-      <Main />
     </div>
   );
 }
