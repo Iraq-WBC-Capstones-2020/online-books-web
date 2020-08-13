1a96d9f1df0e2917878185ffde4ab4fb84b5dfd4
Zainab <zai99nb@gmail.com>
2020-08-13 14:28:45 +0300

    creating the payment page

---
+++ src/pages/PaymentPage/PaymentPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PaymentTable from '../../components/PaymentPage/PaymentTable/PaymentTable';
+import PaymentForm from '../../components/PaymentPage/PaymentForm/PaymentForm'
+import './PaymentPage.scss'
+import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
+
+export default function PaymentPage() {
+  return (
+    <MDBContainer >
+      <MDBRow center className="payform">
+        <MDBCol md="5" >
+          <PaymentTable />
+        </MDBCol>
+        <MDBCol md="5">
+          <PaymentForm />
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+}
