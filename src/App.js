90b937ffd6c2fec71c94f1578fa79e5d2e0042eb
MohammedAlabd <mu96abd@gmail.com>
2020-08-14 13:24:02 +0300

    fix about us page path

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { HashRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './Pages/nav';
 import Footer from './components/Footer/Footer';
 
-import AboutUsPage from './pages/AboutUsPage';
+import AboutUsPage from './pages/AboutUsPage/AboutUsPage';
 import AuthorPage from './pages/AuthorPage/AuthorPage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
 import LoginPage from './pages/loginPage/LoginPage';
@@ -19,11 +19,8 @@ function App() {
       <Switch>
         <Route exact path="/book/:bookId" component={BookPage} />
         <Route path="/aboutus" component={AboutUsPage} />
-
         <Route exact path="/contact" render={ContactUsPage} />
-
         <Route path="/purchase" component={PaymentPage} />
-
         <Route path="/author/:authorId" component={AuthorPage} />
         <Route exact path="/profile" component={ProfilePage} />
         <Route exact path="/login" component={LoginPage} />
