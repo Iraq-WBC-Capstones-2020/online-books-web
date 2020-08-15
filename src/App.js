e8b994b2fdd6a6d9729fa0d5a493956522374717
sara mohammed <sara.korea96@gmail.com>
2020-08-15 03:00:11 +0300

    test pass

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
 import React from 'react';
 import './i18n';
 import { HashRouter, Switch, Route } from 'react-router-dom';
-import Navbar from './Pages/nav';
+import Navbar from './pages/nav';
 import Footer from './components/Footer/Footer';
-
-import AboutUsPage from './pages/AboutUsPage';
+import AboutUsPage from './pages/AboutUsPage/AboutUsPage';
 import AuthorPage from './pages/AuthorPage/AuthorPage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
 import LoginPage from './pages/loginPage/LoginPage';
 import MainPage from './pages/MainPage/MainPage';
-import ContactUsPage from './pages/ContactUsPage';
-
 
 function App() {
   return (
     <HashRouter>
       <Navbar />
       <Switch>
-        <Route exact path="/book/:bookId" component={BookPage} />
+        <Route exact path="/book/:bookId" render={() => <div>BookPage</div>} />
         <Route path="/aboutus" component={AboutUsPage} />
-
-        <Route exact path="/contact" render={ContactUsPage} />
-
-        <Route path="/purchase" component={PaymentPage} />
-
+        <Route exact path="/contact" render={} />
         <Route path="/author/:authorId" component={AuthorPage} />
         <Route exact path="/profile" component={ProfilePage} />
         <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/signup" component={signUp} />
-        <Route exact path="/" render={() => <MainPage />} />
+        <Route exact path="/signup" render={() => <div>SignupPage</div>} />
+        <Route path="/" render={() => <MainPage />} />
       </Switch>
       <Footer />
     </HashRouter>
