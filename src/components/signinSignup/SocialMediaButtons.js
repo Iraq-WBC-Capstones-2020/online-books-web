97670f48eb8a3f633fef0ffcc8e1aa7c4996945a
sara mohammed <sara.korea96@gmail.com>
2020-08-15 04:54:19 +0300

    fixing the login and signup page

--- src/components/signinSignup/SocialMediaButtons.js
+++ src/components/signinSignup/SocialMediaButtons.js
@@ -4,14 +4,15 @@ import { MDBCol, MDBCard, MDBCardBody } from 'mdbreact';
 export default function SocialMediaButtons() {
   return (
     <MDBCol md="5" className="mt-5">
-      <MDBCard className="mt-5 log">
+      <MDBCard className="mt-5 log z-depth-0">
         <MDBCardBody className="mx-4">
           <div className="text-center">
             <div className=" social">
               <button
                 type="button"
                 className="btn btn-gplus
-                   btn btn-danger rounded-pill"
+                   btn btn-danger rounded-pill z-depth-0 animate transition duration-500 ease-in-out  
+                   transform hover:-translate-y-1 hover:scale-110"
               >
                 <i className="fab fa-google-plus-g pr-1"></i> Login with Google
               </button>
@@ -20,7 +21,8 @@ export default function SocialMediaButtons() {
               <button
                 type="button"
                 className="btn btn-git
-                  btn btn-grey rounded-pill"
+                  btn btn-grey rounded-pill z-depth-0 animate transition duration-500 ease-in-out  
+                  transform hover:-translate-y-1 hover:scale-110"
               >
                 <i className="fab fa-github pr-1"></i> Login with Github...
               </button>
@@ -29,7 +31,8 @@ export default function SocialMediaButtons() {
               <button
                 type="button"
                 className="btn btn-fb
-                   btn btn-primary rounded-pill"
+                   btn btn-primary rounded-pill z-depth-0 animate transition duration-500 ease-in-out  
+                   transform hover:-translate-y-1 hover:scale-110"
               >
                 <i className="fab fa-facebook-f pr-1"></i>Login with Facebook
               </button>
