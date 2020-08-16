a34c33dabe1bbe83c17bfbc901330706c4b85fa9
Qays <work.worker55@gmail.com>
2020-08-16 11:31:34 +0300

    Adding the login and signup fuctions

--- src/Pages/SignUpPage/SignUpPage.js
+++ src/Pages/SignUpPage/SignUpPage.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { MDBContainer, MDBRow } from 'mdbreact';
 import '../loginPage/login.scss';
 import Form from '../../components/signinSignup/Form';
 import SocialMediaButtons from '../../components/signinSignup/SocialMediaButtons';
 import VerticalSeparated from '../../components/signinSignup/VerticalSeparated';
+import { usersRef, auth } from './../../api/firebase';
 
 function FormPage() {
+  const formRef = useRef();
+
   const formData = {
+    formRef:formRef,
     title: 'Sign Up',
     button: { title: 'Sign Up', href: '#' },
 
@@ -15,20 +19,46 @@ function FormPage() {
         id: '1',
         label: 'Full Name',
         type: 'text',
+        name:'fullName'
       },
       {
         id: '2',
         label: 'Your Email',
         type: 'email',
+        name:'email'
       },
       {
         id: '3',
         label: 'Your Password',
         type: 'password',
+        name:'password'
       },
     ],
   };
-
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    const form = new FormData(formRef.current);
+    const newUser = {
+      fullName: form.get('fullName'),
+      email: form.get('email'),
+      password: form.get('password'),
+    };
+    try {
+      const userCredential = await auth.createUserWithEmailAndPassword(
+        newUser.email,
+        newUser.password
+      );
+      const newUserFirebase = {
+        uid: userCredential.user.uid,
+        fullName: newUser.fullName,
+        email: newUser.email,
+      };
+      await usersRef.doc(newUserFirebase.uid).set(newUserFirebase);
+      // onClose();
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
   return (
     <MDBContainer>
       <MDBRow>
--- src/Pages/loginPage/LoginPage.js
+++ src/Pages/loginPage/LoginPage.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import './login.scss';
 import Form from '../../components/signinSignup/Form';
 import SocialMediaButtons from '../../components/signinSignup/SocialMediaButtons';
 import { MDBContainer, MDBRow } from 'mdbreact';
 import VerticalSeparated from '../../components/signinSignup/VerticalSeparated';
+import { auth } from './../../api/firebase';
 
 function FormPage() {
+  const formRef = useRef();
+
   const formData = {
+    formRef:formRef,
     title: 'Sign in',
     button: { title: 'Sign in', href: '#' },
     extraAnchor: {
@@ -18,13 +22,29 @@ function FormPage() {
         id: '1',
         label: 'Your Email',
         type: 'email',
+        name:'email'
       },
       {
         id: '2',
         label: 'Your Password',
         type: 'password',
+        name:'password'
       },
     ],
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const form = new FormData(formRef.current);
+    try {
+      const userCredential = await auth.signInWithEmailAndPassword(
+        form.get('email'),
+        form.get('password')
+      );
+      // if (userCredential.user) onClose();
+    } catch (e) {
+      console.log(e.message);
+    }
   };
 
   return (
