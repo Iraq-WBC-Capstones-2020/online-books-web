944ac8c0d9c381b3c2c545f6791e7f50ced3d915
Qays <work.worker55@gmail.com>
2020-08-17 09:12:03 +0300

    using i18n in the nacbar

--- src/Pages/nav.js
+++ src/Pages/nav.js
@@ -117,7 +117,7 @@ function NavbarPage() {
                       className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mx-2 rounded-full"
                       onClick={onLogout}
                     >
-                      Logout
+                      {t('navBar.logout')}
                     </button>
                   </MDBNavItem>
                 </>
@@ -129,7 +129,7 @@ function NavbarPage() {
                       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 border-2 border-transparent rounded-full"
                       onClick={goToSignUpPage}
                     >
-                      Sign Up
+                      {t('navBar.signUp')}
                     </button>
                   </MDBNavItem>
                   <MDBNavItem>
@@ -137,7 +137,7 @@ function NavbarPage() {
                       className="bg-gray-100 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border-2 border-blue-500 rounded-full"
                       onClick={goToLoginPage}
                     >
-                      Login
+                      {t('navBar.login')}
                     </button>
                   </MDBNavItem>
                 </>
