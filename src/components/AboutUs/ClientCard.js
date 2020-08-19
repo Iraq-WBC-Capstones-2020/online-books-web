2e6504f03687b03047da395d6f7b3a5146136876
Qays <work.worker55@gmail.com>
2020-08-19 10:12:16 +0300

    Client section

--- src/components/AboutUs/ClientCard.js
+++ src/components/AboutUs/ClientCard.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 function ClientCard({ client }) {
   return (
     <>
-      <div className="border border-solid border-4 border-gray-200 flex justify-center">
+      <div className="flex justify-center">
         <img
           src={client.logo}
           alt={client.name}
           className="animate transition duration-500 ease-in-out  
-                        transform hover:-translate-y-1 hover:scale-110 w-1/3 m-4"
+                        transform hover:-translate-y-1 hover:scale-110 w-1/2 m-2"
         />
       </div>
     </>
