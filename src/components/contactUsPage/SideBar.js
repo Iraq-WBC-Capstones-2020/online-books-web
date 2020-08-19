fd7ddefe9262b8deeebb8285feb82850c3cb2915
Zainab <zai99nb@gmail.com>
2020-08-19 15:28:29 +0300

    making the contact page responsive

--- src/components/contactUsPage/SideBar.js
+++ src/components/contactUsPage/SideBar.js
@@ -18,7 +18,7 @@ function ContactInformation() {
             className="flex justify-start animate transition duration-500 ease-in-out  
                         transform hover:-translate-y-1 hover:scale-110 ml-10 m-4"
           >
-            <i className="fas fa-map-marker-alt text-4xl white-text ml-24"></i>
+            <i className="fas fa-map-marker-alt text-4xl white-text ml-18"></i>
             <p className="text-gray-400 ml-4">
               A108 Adam Street
               <br /> New York NY 535022
@@ -28,8 +28,8 @@ function ContactInformation() {
             className="flex justify-start animate transition duration-500 ease-in-out  
                         transform hover:-translate-y-1 hover:scale-110  m-4"
           >
-            <i className="fas fa-envelope pr-2 text-4xl white-text ml-24"></i>
-            <p className="text-gray-400 ml-2">
+            <i className="fas fa-envelope pr-2 text-4xl white-text ml-18"></i>
+            <p className="text-gray-400 ml-1 mr-1">
               contact@example.com
               <br />
               info@example.com
@@ -39,7 +39,7 @@ function ContactInformation() {
             className="flex justify-start animate transition duration-500 ease-in-out  
                         transform hover:-translate-y-1 hover:scale-110  m-4"
           >
-            <i className="fas fa-phone pr-2 text-4xl white-text ml-24"></i>
+            <i className="fas fa-phone pr-2 text-4xl white-text ml-18"></i>
             <p className="text-gray-400 ml-2 mb-10">
               + 01 234 567 89 <br />
               +1 5589 55488 51
