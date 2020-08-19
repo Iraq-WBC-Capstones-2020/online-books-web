dceacd6faab6b19f7b44f9f2a237dbec83dfdb18
Qays <work.worker55@gmail.com>
2020-08-19 08:05:37 +0300

    chaning margin into padding for better ui

--- src/components/SectionTitle/SectionTitle.js
+++ src/components/SectionTitle/SectionTitle.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function SectionTitle({ children }) {
   return (
-    <div className="text-center mt-5">
+    <div className="text-center py-3">
       <h1 className="text-3xl font-medium inline border-b border-gray-400 px-5">
         {children}
       </h1>
