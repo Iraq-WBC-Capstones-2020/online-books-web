ed111cdd0a337b145677f029bf1e87aff045bfcd
Neven Bedr <neven2225@gmail.com>
2020-08-21 18:15:47 +0300

    changing the name

--- src/components/AudioPage/__test__/audioPage.test.js
+++ src/components/AudioPage/__test__/audioPage.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import AudioPageSection from './../audioPage';
+import AudioPageSection from '../AudioPage';
 
 test('audioPage snapshot', () => {
   const tree = renderer.create(<AudioPageSection />).toJSON();
