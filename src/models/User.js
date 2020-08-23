d0d473fdbfea7c1dcd3fb15000f1f221eb922985
Qays <work.worker55@gmail.com>
2020-08-23 16:16:08 +0300

    adding getBooksInCart for user

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import { usersRef } from './../api/firebase';
+import Book from './Book';
 
 export default class User {
   constructor({ id, name, age, email, birthday, image, isSigned = true } = {}) {
@@ -14,9 +15,12 @@ export default class User {
     const doc = await usersRef.doc(id).get();
     return new User(doc.data());
   }
-
+  async getBooksInCart() {
+    const books = await usersRef.doc(this.id).collection('cart').get();
+    return books.docs.map((book) => new Book(book));
+  }
   updateUserInfo() {
-    if (!this.id) this.id= usersRef.doc().id;
-    usersRef.doc(this.id).set(this);
+    if (!this.id) this.id = usersRef.doc().id;
+    usersRef.doc(this.id).set({ ...this });
   }
 }
