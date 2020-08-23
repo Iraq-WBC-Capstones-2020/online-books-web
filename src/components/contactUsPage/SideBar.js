de4c74dfc22ddd56a5b556f1cba18449389cfd75
Dunia <dunia.nasr777@gmail.com>
2020-08-23 17:34:29 +0300

    fixing sidebar

--- src/components/contactUsPage/SideBar.js
+++ src/components/contactUsPage/SideBar.js
@@ -6,24 +6,16 @@ function ContactInformation() {
 
   return (
     <>
-
       <div className="flex justify-center mt-5">
         <MDBCard className="max-h-full bg-blue-800 w-1/4 flex justify-center">
-
-      <div className="flex justify-end">
-        <MDBCard
-          color="blue darken-3"
-          className=" w-75 h-100 flex justify-center"
-        >
-
           <h1 className=" my-4 pb-2 text-center white-text text-2xl">
             {t('contactUsPage.contactUs')}
           </h1>
           <div
             className="flex justify-start animate transition duration-500 ease-in-out  
-                        transform hover:-translate-y-1 hover:scale-110 ml-10 m-4"
+                        transform hover:-translate-y-1 hover:scale-110 m-4"
           >
-            <i className="fas fa-map-marker-alt text-4xl white-text ml-18"></i>
+            <i className="fas fa-map-marker-alt text-4xl white-text ml-16"></i>
             <p className="text-gray-400 ml-4">
               A108 Adam Street
               <br /> New York NY 535022
@@ -31,10 +23,10 @@ function ContactInformation() {
           </div>
           <div
             className="flex justify-start animate transition duration-500 ease-in-out  
-                        transform hover:-translate-y-1 hover:scale-110  m-4"
+                        transform hover:-translate-y-1 hover:scale-110 m-4"
           >
-            <i className="fas fa-envelope pr-2 text-4xl white-text ml-18"></i>
-            <p className="text-gray-400 ml-1 mr-1">
+            <i className="fas fa-envelope pr-2 text-4xl white-text ml-16"></i>
+            <p className="text-gray-400 ml-2">
               contact@example.com
               <br />
               info@example.com
@@ -42,10 +34,10 @@ function ContactInformation() {
           </div>
           <div
             className="flex justify-start animate transition duration-500 ease-in-out  
-                        transform hover:-translate-y-1 hover:scale-110  m-4"
+                        transform hover:-translate-y-1 hover:scale-110 m-4"
           >
-            <i className="fas fa-phone pr-2 text-4xl white-text ml-18"></i>
-            <p className="text-gray-400 ml-2 mb-10">
+            <i className="fas fa-phone pr-2 text-4xl white-text ml-16"></i>
+            <p className="text-gray-400 ml-2 mb-24">
               + 01 234 567 89 <br />
               +1 5589 55488 51
             </p>
