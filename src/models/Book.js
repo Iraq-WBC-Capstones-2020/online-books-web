a8cbdd3819148db6da341d4ba1871a2c458c2c62
Qays <work.worker55@gmail.com>
2020-08-23 17:11:42 +0300

    add rating to book class

--- src/models/Book.js
+++ src/models/Book.js
@@ -1,7 +1,24 @@
-import { booksRef } from './../api/firebase';
-import Author from "./Author";
+import { booksRef, authorsRef, usersRef } from './../api/firebase';
+import Author from './Author';
+
 export default class Book {
-    constructor({ id, name, type, price, dis_count, cover, have_audio, description, audio_ref, author_id, ref, tags } = {}) {
+  constructor({
+    id,
+    name = null,
+    type = null,
+    price = null,
+    dis_count = null,
+    cover = null,
+    have_audio = null,
+    description = null,
+    audio_ref = null,
+    author_id = null,
+    author_name = null,
+    views = null,
+    rating = null,
+    ref = null,
+    tags = null,
+  } = {}) {
         this.id = id;
         this.name = name;
         this.type = type;
@@ -12,11 +29,14 @@ export default class Book {
         this.description = description;
         this.audio_ref = audio_ref;
         this.author_id = author_id;
+    this.author_name = author_name;
+    this.views = views;
+    this.rating = rating;
         this.ref = ref;
         this.tags = tags;
     }
     async getAuthor() {
-        return await Author.getAuthorFromDB(author_id);
+    return await Author.getAuthorFromDB(this.author_id);
     }
     static async getBookFromDB(id) {
         const doc = await booksRef.doc(id).get();
@@ -36,6 +56,20 @@ export default class Book {
     }
     updateBookInfo() {
         if (!this.id) this.id = booksRef.doc().id;
-        booksRef.doc(this.id).set(this);
+    booksRef.doc(this.id).set({ ...this });
+  }
+  static async sendBooks() {
+    console.log(books);
+    for (const book of books) {
+      console.log(book);
+      const author = (
+        await authorsRef.where('name', '==', book.authorName).limit(1).get()
+      ).docs[0].data();
+      book.author_id = author.id;
+      book.author_name = author.name;
+      const oneBook = new Book(book);
+      console.log(oneBook);
+      await oneBook.updateBookInfo();
+    }
     }
 }
