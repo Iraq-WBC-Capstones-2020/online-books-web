009b94da2aeb038a37baadb741fcf2653387e33e
MohammedAlabd <mu96abd@gmail.com>
2020-08-23 23:28:12 +0300

    fix the contact page sidebar

--- src/components/contactUsPage/SideBar.js
+++ src/components/contactUsPage/SideBar.js
@@ -7,7 +7,7 @@ function ContactInformation() {
   return (
     <>
       <div className="flex justify-center mt-5">
-        <MDBCard className="max-h-full bg-blue-800 w-1/4 flex justify-center">
+        <MDBCard className="max-h-full bg-blue-800 flex justify-center w-full">
           <h1 className=" my-4 pb-2 text-center white-text text-2xl">
             {t('contactUsPage.contactUs')}
           </h1>
@@ -15,7 +15,7 @@ function ContactInformation() {
             className="flex justify-start animate transition duration-500 ease-in-out  
                         transform hover:-translate-y-1 hover:scale-110 m-4"
           >
-            <i className="fas fa-map-marker-alt text-4xl white-text ml-16"></i>
+            <i className="fas fa-map-marker-alt text-4xl white-text lg:ml-16"></i>
             <p className="text-gray-400 ml-4">
               A108 Adam Street
               <br /> New York NY 535022
@@ -25,7 +25,7 @@ function ContactInformation() {
             className="flex justify-start animate transition duration-500 ease-in-out  
                         transform hover:-translate-y-1 hover:scale-110 m-4"
           >
-            <i className="fas fa-envelope pr-2 text-4xl white-text ml-16"></i>
+            <i className="fas fa-envelope pr-2 text-4xl white-text lg:ml-16"></i>
             <p className="text-gray-400 ml-2">
               contact@example.com
               <br />
@@ -36,7 +36,7 @@ function ContactInformation() {
             className="flex justify-start animate transition duration-500 ease-in-out  
                         transform hover:-translate-y-1 hover:scale-110 m-4"
           >
-            <i className="fas fa-phone pr-2 text-4xl white-text ml-16"></i>
+            <i className="fas fa-phone pr-2 text-4xl white-text lg:ml-16"></i>
             <p className="text-gray-400 ml-2 mb-24">
               + 01 234 567 89 <br />
               +1 5589 55488 51
