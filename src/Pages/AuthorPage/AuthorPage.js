9df674ee2818e30c91eee45e4fe2ae644bcd6e91
Qays <work.worker55@gmail.com>
2020-08-23 10:55:39 +0300

    connecting author page with the db

--- src/Pages/AuthorPage/AuthorPage.js
+++ src/Pages/AuthorPage/AuthorPage.js
@@ -1,60 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdbreact';
 import { useTranslation } from 'react-i18next';
 import Paragraph from '../../components/Paragraph/Paragraph';
 import SectionTitle from '../../components/SectionTitle/SectionTitle';
+import { useParams } from 'react-router-dom';
+import Author from '../../models/Author';
+import Spinner from '../../components/Spinner/Spinner';
 
 function AuthorPage() {
   const { t } = useTranslation();
-  const name = 'Ann Druyan';
-  const firstName = name.split(' ')[0];
-  const paragraph =
-    'Ann Druyan is an Emmy and Peabody Award-winning writer and producer who has dedicated her life to science. In 1980, Ann, alongside late husband Carl Sagan, wrote the 1980 original COSMOS: A Personal Voyage — which was a 13-part television event on PBS. COSMOS is still one of the most-watched programs in the history of PBS and its airing was a major moment for science-themed television.';
-  const authorPageImg = 'https://via.placeholder.com/300x600';
-  const socialMediaAccounts = [
-    { name: 'LinkedIn', icon: 'linkedin', link: 'https://www.linkedin.com' },
-    {
-      name: 'Facebook',
-      icon: 'facebook-square',
-      link: 'https://www.facebook.com',
-    },
-    {
-      name: 'Twitter',
-      icon: 'twitter-square',
-      link: 'https://www.twitter.com',
-    },
-  ];
-  const followText = `${t('authorPage.follow')} ${firstName}`;
-  const authorBookText = `${firstName}'s ${t('authorPage.books')}`;
+  const { authorId } = useParams();
+  const [author, setAuthor] = useState();
+  useEffect(() => {
+    (async () => {
+      const author = await Author.getAuthorFromDB(authorId);
+      setAuthor(author);
+    })();
+  }, [authorId]);
+  if (author) {
+    var name = author.name;
+    var firstName = name.split(' ')[0];
+    var paragraph = author.bio;
+    var authorPageImg = author.avatar;
+    var socialMediaAccounts = author.socialMedia;
+    var followText = `${t('authorPage.follow')} ${firstName}`;
+    var authorBookText = `${firstName}'s ${t('authorPage.books')}`;
+  }
   return (
     <>
-      <div className="bg-gray-200">
-        <MDBContainer>
-          <MDBRow className="h-auto">
-            <MDBCol lg="7" md="8" sm="9" size="12" className="py-4">
-              <Paragraph title={name + ':'}>{paragraph}</Paragraph>
-              <div className="pb-4 text-xl">
-                <p className="mt-8 font-medium">{followText}</p>
-                <p className="text-2xl">
-                  {socialMediaAccounts.map((account) => (
-                    <a key={account.name} href={account.link}>
-                      <MDBIcon fab icon={account.icon} className="mr-3" />
-                    </a>
-                  ))}
-                </p>
-              </div>
-            </MDBCol>
-            <MDBCol lg="5" md="4" sm="3" size="12" className="pt-5 pb-4">
-              <img
-                src={authorPageImg}
-                alt={name}
-                className="rounded-lg mx-auto h-auto max-w-full"
-              />
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-      </div>
-      <SectionTitle>{authorBookText}</SectionTitle>
+      {!author ? (
+        <Spinner />
+      ) : (
+        <>
+          <div className="bg-gray-200">
+            <MDBContainer>
+              <MDBRow className="h-auto">
+                <MDBCol lg="7" md="8" sm="9" size="12" className="py-4">
+                  <Paragraph title={name + ':'}>{paragraph}</Paragraph>
+                  {socialMediaAccounts && socialMediaAccounts.length != 0 && (
+                    <div className="pb-4 text-xl">
+                      <p className="mt-8 font-medium">{followText}</p>
+                      <p className="text-2xl">
+                        {socialMediaAccounts.map((account) => (
+                          <a key={account.name} href={account.link}>
+                            <MDBIcon fab icon={account.icon} className="mr-3" />
+                          </a>
+                        ))}
+                      </p>
+                    </div>
+                  )}
+                </MDBCol>
+                <MDBCol lg="5" md="4" sm="3" size="12" className="pt-5 pb-4">
+                  <img
+                    src={authorPageImg}
+                    alt={author.name}
+                    className="rounded-lg mx-auto h-auto max-w-full"
+                  />
+                </MDBCol>
+              </MDBRow>
+            </MDBContainer>
+          </div>
+          <SectionTitle>{authorBookText}</SectionTitle>
+        </>
+      )}
     </>
   );
 }
