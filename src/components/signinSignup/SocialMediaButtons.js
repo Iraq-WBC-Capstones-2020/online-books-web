190638b59f5cd2d9e769e737ef6721182bae7c10
Dunia <dunia.nasr777@gmail.com>
2020-08-23 17:55:01 +0300

    fixing errors

--- src/components/signinSignup/SocialMediaButtons.js
+++ src/components/signinSignup/SocialMediaButtons.js
@@ -14,76 +14,80 @@ export default function SocialMediaButtons() {
   const signInWithGithub = () => auth.signInWithPopup(githubAuthProvider);
   const signInWithFacebook = () => auth.signInWithPopup(facebookAuthProvider);
   return (
+    <>
+      <MDBCol md="5" className="mt-5">
+        <MDBCard className="mt-5 log z-depth-0">
+          <MDBCardBody className="mx-4">
+            <div className="text-center">
+              <div className=" social text-white">
+                <button
+                  type="button"
+                  className="w-56 btn btn-gplus
 
-    <MDBCol md="5" className="mt-5">
+             btn btn-danger rounded-pill"
+                >
+                  <i className="fab fa-google-plus-g pr-1"></i>
+                  Login with Google
+                </button>
+                <br></br>
+                <button
+                  type="button"
+                  className="w-56 btn btn-git
+                        btn btn-grey rounded-pill text-white"
+                >
+                  <i className="fab fa-github pr-1 text-white "></i> Login with
+                  Github...
+                </button>
+                <br></br>
+
+                <button
+                  type="button"
+                  className="w-56 btn btn-fb
+                 btn btn-primary rounded-pill"
+                >
+                  <i className="fab fa-facebook-f pr-1"></i>
+                  Login with Facebook
+                </button>
+              </div>
+            </div>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+
       <MDBCard className="mt-5 log z-depth-0">
         <MDBCardBody className="mx-4">
           <div className="text-center">
-            <div className=" social text-white">
+            <div className="social text-white">
               <button
                 type="button"
-                className="w-56 btn btn-gplus
-
-             btn btn-danger rounded-pill"
+                className="btn btn-gplus btn-danger rounded-pill"
+                onClick={signInWithGoogle}
               >
-                <i className="fab fa-google-plus-g pr-1"></i>
-                Login with Google
+                <i className="fab fa-google-plus-g pr-1"></i>{' '}
+                {t('socialMediaPage.signInGoogle')}
               </button>
               <br></br>
               <button
                 type="button"
-                className="w-56 btn btn-git
-                        btn btn-grey rounded-pill text-white"
+                className="btn btn-git btn-grey rounded-pill text-white"
+                onClick={signInWithGithub}
               >
-                <i className="fab fa-github pr-1 text-white "></i> Login with
-                Github...
+                <i className="fab fa-github pr-1 text-white"></i>{' '}
+                {t('socialMediaPage.signInGithub')}
               </button>
               <br></br>
-
               <button
                 type="button"
-                className="w-56 btn btn-fb
-                 btn btn-primary rounded-pill"
+                className="btn btn-fb btn-primary rounded-pill"
+                onClick={signInWithFacebook}
               >
-                <i className="fab fa-facebook-f pr-1"></i>
-                Login with Facebook
+                <i className="fab fa-facebook-f pr-1"></i>{' '}
+                {t('socialMediaPage.signInFacebook')}
               </button>
             </div>
-
-    <MDBCard className="mt-5 log z-depth-0">
-      <MDBCardBody className="mx-4">
-        <div className="text-center">
-          <div className="social text-white">
-            <button
-              type="button"
-              className="btn btn-gplus btn-danger rounded-pill"
-              onClick={signInWithGoogle}
-            >
-              <i className="fab fa-google-plus-g pr-1"></i>{' '}
-              {t('socialMediaPage.signInGoogle')}
-            </button>
-            <br></br>
-            <button
-              type="button"
-              className="btn btn-git btn-grey rounded-pill text-white"
-              onClick={signInWithGithub}
-            >
-              <i className="fab fa-github pr-1 text-white"></i>{' '}
-              {t('socialMediaPage.signInGithub')}
-            </button>
-            <br></br>
-            <button
-              type="button"
-              className="btn btn-fb btn-primary rounded-pill"
-              onClick={signInWithFacebook}
-            >
-              <i className="fab fa-facebook-f pr-1"></i>{' '}
-              {t('socialMediaPage.signInFacebook')}
-            </button>
-
           </div>
-        </div>
-      </MDBCardBody>
-    </MDBCard>
+        </MDBCardBody>
+      </MDBCard>
+    </>
   );
 }
